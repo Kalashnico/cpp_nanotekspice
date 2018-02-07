e618fc89789f1c86186b2d8e28c3d22a54220846
Jeremiah DECOMBE <jeremiah.decombe@epitech.eu>
2018-02-07 11:35:12 +0100

    Recursive algorithm seems to work for basic tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,23 @@
 
 int main() {
 	nts::Component4001 c1("c1");
-	nts::Component4001 c2("c2");
 	nts::ComponentInput in1("input", nts::Tristate::FALSE);
 	nts::ComponentInput in2("input", nts::Tristate::FALSE);
+	nts::ComponentInput in3("input", nts::Tristate::FALSE);
+	nts::ComponentInput in4("input", nts::Tristate::FALSE);
 
 	c1.setLink(1, in1, 1);
 	c1.setLink(2, in2, 1);
+	c1.setLink(4, in2, 1);
+	c1.setLink(5, in2, 1);
+
+	c1.setLink(3, c1, 8);
+	c1.setLink(6, c1, 9);
+
 	c1.dump();
 	in1.dump();
 	in2.dump();
-	if (c1.compute(3) == nts::Tristate::TRUE)
+	if (c1.compute(10) == nts::Tristate::TRUE)
 		std::cout << "toto" << std::endl;
 	return 0;
 }
