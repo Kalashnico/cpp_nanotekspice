984f4dacd18c7f2fd2487f7f2f9619f04c32b9b7
Nicolas Guerin <nicolas.guerin@epitech.eu>
2018-02-09 09:20:00 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <histedit.h>
 #include "parsing/Token.hpp"
 #include "parsing/Parser.hpp"
 
@@ -6,7 +7,7 @@ int main() {
     token::TokenList tokenList;
     std::string buffer;
 
-    buffer = parsing::Parser::openFile("and.nts");
+    buffer = parsing::Parser::openFile("test");
 
     tokenList.Tokenizer(buffer);
 
