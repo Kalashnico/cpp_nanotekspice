0ad7fd5a194b3f7b60f7e310ea7ddae153cb78cc
Jeremiah DECOMBE <jeremiah.decombe@epitech.eu>
2018-02-28 13:53:39 +0100

    False default state for Clock

--- src/Component/ComponentClock.cpp
+++ src/Component/ComponentClock.cpp
@@ -30,5 +30,5 @@ nts::Tristate nts::ComponentClock::compute(size_t pin)
 
 nts::ComponentClock *nts::CreateClock(const std::string &name)
 {
-        return new nts::ComponentClock(name, nts::Tristate::UNDEFINED);
+        return new nts::ComponentClock(name, nts::Tristate::FALSE);
 }
