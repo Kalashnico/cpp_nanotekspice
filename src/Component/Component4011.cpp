7c337ccb43827095f995dc0ed690c38fbb864830
Jeremiah DECOMBE <jeremiah.decombe@epitech.eu>
2018-02-28 14:04:36 +0100

    Fix 4011

--- src/Component/Component4011.cpp
+++ src/Component/Component4011.cpp
@@ -12,9 +12,9 @@ nts::Component4011::Component4011(std::string name)
     this->_pins[1].type = pin::INPUT;
     this->_pins[2].type = pin::OUTPUT;
 
-    this->_pins[3].type = pin::INPUT;
+    this->_pins[3].type = pin::OUTPUT;
     this->_pins[4].type = pin::INPUT;
-    this->_pins[5].type = pin::OUTPUT;
+    this->_pins[5].type = pin::INPUT;
 
     this->_pins[6].type = pin::UNUSED;
 
@@ -22,9 +22,9 @@ nts::Component4011::Component4011(std::string name)
     this->_pins[8].type = pin::INPUT;
     this->_pins[9].type = pin::OUTPUT;
 
-    this->_pins[10].type = pin::INPUT;
+    this->_pins[10].type = pin::OUTPUT;
     this->_pins[11].type = pin::INPUT;
-    this->_pins[12].type = pin::OUTPUT;
+    this->_pins[12].type = pin::INPUT;
 
     this->_pins[13].type = pin::UNUSED;
 
@@ -43,8 +43,12 @@ nts::Tristate nts::Component4011::compute(size_t pin)
         return _pins[pin].value;
     _pins[pin].isDirty = false;
     if (_pins[pin].type == pin::OUTPUT) {
-        _pins[pin].value = nand_gate(this->compute(pin),
-                                  this->compute(pin - 1));
+        if (pin == 2 || pin == 9)
+            _pins[pin].value = nand_gate(this->compute(pin),
+                                      this->compute(pin - 1));
+        else
+            _pins[pin].value = nand_gate(this->compute(pin + 2),
+                    this->compute(pin + 3));
     } else if (_pins[pin].type == pin::INPUT) {
         _pins[pin].value = _pins[pin].otherPin->owner->compute(
                 _pins[pin].otherPin->pos);
